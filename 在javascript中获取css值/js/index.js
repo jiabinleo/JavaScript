bba3195476845492ecb7bb3a038fb3c6454b2429
jiabinleo <jiabinleo@126.com>
2018-07-31 14:01:12 +0800

    获取内联样式

--- 在javascript中获取css值/js/index.js
+++ 在javascript中获取css值/js/index.js
@@ -1,3 +1,10 @@
-(function(){
+window.onload=function(){
+    const element = document.querySelector('.element');
     
-})()
+    const fontSize = element.style.fontSize;
+    console.log(fontSize)
+
+    const color = element.style.color;
+    console.log(color)
+
+}
