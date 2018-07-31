bf901e0e57b017f205b2c356773ba0880ff4219f
jiabinleo <jiabinleo@126.com>
2018-07-31 14:10:00 +0800

    获取计算样式

--- 在javascript中获取css值/js/index.js
+++ 在javascript中获取css值/js/index.js
@@ -1,10 +1,16 @@
 window.onload=function(){
     const element = document.querySelector('.element');
-    
+
     const fontSize = element.style.fontSize;
-    console.log(fontSize)
+    console.log(fontSize);
 
     const color = element.style.color;
-    console.log(color)
+    console.log(color);
+    
 
+    const style = getComputedStyle(element);
+    console.log(style)
+
+    const backgroundColor = style.backgroundColor;
+    console.log(backgroundColor)
 }
